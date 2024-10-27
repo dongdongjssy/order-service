7886a98a8f23213912ab212490e603c33fd56909
Dong Dong <ddong@nanthealth.com>
2024-10-27 15:01:34 -0400

    fix issue where required fields are not validated

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,10 +1,13 @@
 package handlers
 
 import (
+	"fmt"
 	"net/http"
+	"unicode"
 
 	"github.com/dongdongjssy/order-service/model"
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
 	"github.com/go-playground/validator/v10"
 )
 
@@ -12,6 +15,7 @@ const (
 	API_SUCCESS = "success"
 
 	ERR_INVALID_BODY     = "failed to parse request body"
+	ERR_INVALID_FIELD    = "error in filed '%s': %s"
 	ERR_DUPLICATED_ORDER = "duplicated order found"
 )
 
@@ -19,18 +23,25 @@ func TransformOrders(ctx *gin.Context) {
 	// parse request body
 	var orders []model.Order
 	if err := ctx.ShouldBindJSON(&orders); err != nil {
-		// if find error, return parse error details
-		if validationErrs, ok := err.(validator.ValidationErrors); ok {
-			errors := make(map[string]string)
-			for _, e := range validationErrs {
-				errors[e.Field()] = e.Tag()
+		// response error details
+		if sliceValidationErrs, ok := err.(binding.SliceValidationError); ok {
+			var errors []string
+			for _, sve := range sliceValidationErrs {
+				if validationErrs, ok := sve.(validator.ValidationErrors); ok {
+					for _, ve := range validationErrs {
+						errors = append(errors, fmt.Sprintf(ERR_INVALID_FIELD, toLowerFirstChar(ve.Field()), ve.Tag()))
+					}
+				}
 			}
-			ctx.JSON(http.StatusBadRequest, model.Response{
-				Code:    http.StatusBadRequest,
-				Message: ERR_INVALID_BODY,
-				Error:   errors,
-			})
-			return
+
+			if len(errors) > 0 {
+				ctx.JSON(http.StatusBadRequest, model.Response{
+					Code:    http.StatusBadRequest,
+					Message: ERR_INVALID_BODY,
+					Errors:  errors,
+				})
+				return
+			}
 		}
 	}
 
@@ -45,7 +56,7 @@ func TransformOrders(ctx *gin.Context) {
 			CustomerId:          order.CustomerId,
 			NbrOfPurchasedItems: len(order.Items),
 			Items:               order.Items,
-			TotalAmountEur:      reduceAmount(order.Items, 0, func(acc float64, i model.Item) float64 { return acc + i.CostEur }),
+			TotalAmountEur:      reduceAmount(&order.Items, 0, func(acc float64, i *model.Item) float64 { return acc + i.CostEur }),
 		}
 
 		summaries[index] = summary
@@ -58,14 +69,22 @@ func TransformOrders(ctx *gin.Context) {
 	})
 }
 
-func reduceAmount(
-	array []model.Item,
-	initial float64,
-	f func(float64, model.Item) float64,
+func reduceAmount(array *[]model.Item, initial float64,
+	f func(float64, *model.Item) float64,
 ) float64 {
 	result := initial
-	for _, v := range array {
-		result = f(result, v)
+	for _, v := range *array {
+		result = f(result, &v)
 	}
 	return result
+}
+
+func toLowerFirstChar(str string) string {
+	if len(str) == 0 {
+		return str
+	}
+
+	strSlice := []rune(str)
+	strSlice[0] = unicode.ToLower(strSlice[0])
+	return string(strSlice)
 }
--- model/response.go
+++ model/response.go
@@ -1,8 +1,8 @@
 package model
 
 type Response struct {
-	Code    int         `json:"code" binding:"required"`
-	Message string      `json:"message" binding:"required"`
-	Data    []Summary   `json:"data,omitempty"`
-	Error   interface{} `json:"error,omitempty"`
+	Code    int       `json:"code" binding:"required"`
+	Message string    `json:"message" binding:"required"`
+	Data    []Summary `json:"data,omitempty"`
+	Errors  []string  `json:"errors,omitempty"`
 }
